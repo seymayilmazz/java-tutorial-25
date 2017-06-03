de5a54a1853babf3348d2a5803cbfff09cd9651e
Şeyma Yılmaz <seyma.yilmaz3@ogr.sakarya.edu.tr>
2017-06-04 00:19:27 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+/**
+ * Created by Şeyma Yılmaz on 3.6.2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Şeyma");
+        arrayList.add("Burak");
+        arrayList.add("Berkan");
+
+        arrayList.get(2);
+        /* ... */
+
+        LinkedList linkedList = new LinkedList();
+        linkedList.add("Şeyma");
+        linkedList.add("Burak");
+        linkedList.add("Berkan");
+
+        System.out.println();
+    }
+
+}
